2730564f4ced393e3e6af89b67e6c61e853ab39c
Matias Palladino <matias.palladino@gmail.com>
2021-07-27 21:57:31 -0300

    Versión 0.1 - Se añade NavBar.jsx

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import { FiShoppingCart } from "react-icons/fi";
+
+
+function NavBar(){
+    return (
+        <Navbar bg="dark" variant="dark" sticky="top">
+            <Container fluid>
+                <Navbar.Brand href="#home">
+                    <img
+                    alt="Logo de Infinity-Box"
+                    src="iboxlogo.png"
+                    width="60"
+                    height="60"
+                    className="d-inline-block"
+                    />{' '}
+                Infinity-Box
+                </Navbar.Brand>
+                <Nav className="justify-content-end">
+                    <Nav.Link href="#home">Productos</Nav.Link>
+                    <Nav.Link href="#aboutus">Nosotros</Nav.Link>
+                    <Nav.Link href="#contact">Contacto</Nav.Link>
+                    <Nav.Link href="#cart"><FiShoppingCart /></Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
+export default NavBar
